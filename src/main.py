da1a265eadbab94a513897f189d12d2bda5e59fb
Sorrow-Work <61646954+Sorrow-Scarlet@users.noreply.github.com>
2025-10-24 14:40:01 +0800

    feat: :sparkles: 完成UI
    借助各大AI与不断修正，完成UI设计

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import flask
-
+import modules.util
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return flask.render_template("app.html", title="Home")
+    return flask.render_template("app.html", title="Luna Fox Index")
 
 
 if __name__ == "__main__":
+    util = modules.util.drawHelper(app)
     app.run(debug=True)
---
+++ src/modules/util.py
@@ -0,0 +1,9 @@
+import flask
+
+
+class drawHelper:
+    def __init__(self, app: flask.Flask):
+        self.app = app
+
+    def draw_object(self):
+        pass
